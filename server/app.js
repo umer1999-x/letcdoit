1a094ce02d9e8ed4c53db3424b107baf290c3e49
Usman Yousaf <usmanyousafali@gmail.com>
2021-06-30 19:58:53 +0500

    changed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-// const cors = require('cors')
+const cors = require('cors')
 require("dotenv").config()
 
 
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
