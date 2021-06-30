bc64060ddbb5b3704f97f56f6c65a1bee6e9f583
Usman Yousaf <usmanyousafali@gmail.com>
2021-06-30 19:49:00 +0500

    Chnaged

--- server/routines/routines.js
+++ server/routines/routines.js
@@ -43,8 +43,8 @@ async function sendMail(email,password) {
             from: "M.Umer Sheikh <sheikhumer1999@gmail.com>",
             to: email,
             subject: "You Are The Member!",
-            text: `your email is : ${email} and password is :${password} `,
-
+            // text: `your email is : ${email} and password is :${password} `,
+            html: createEmailHTML(email, password)
         };
 
         const result = await transport.sendMail(mailOptions);
@@ -84,6 +84,35 @@ function Adduser(req, res) {
 
 }
 
+
+function createEmailHTML(email, password) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <style>
+            * {
+                text-align: center;
+                font-family: sans-serif;
+            }
+        </style>
+        <body>
+            <div>
+                <img height="100" src="http://18.118.173.214/images/limlinks/logo.png">
+                <h1>Thank You!</h1>
+                <h3>This is your credentials</h3>
+                <h5><strong>Email: </strong>${email}</h5>
+                <h5><strong>Password: </strong>${password}</h5>
+            </div>   
+        </body>
+    </html>
+    `
+};
 
 
 module.exports = {
